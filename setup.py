711b905b78c4c80f038184e90ae6dacef7f8343e
Karl Schmidt <kschmidt@bluerocktx.com>
2023-10-03 21:11:24 -0400

    fixed file pkg

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='auxdoc',
       author='Karl Schmidt',
       author_email='kfschmidt@gmail.com',
       url='https://github.com/kfschmidt2/auxdoc',
-      packages=['auxdoc', 'auxdoc.media', 'auxdoc.layouts'],
+      packages={"auxdoc"},
       packages_dir={"":"auxdoc"},
       include_package_data=True,
       )
